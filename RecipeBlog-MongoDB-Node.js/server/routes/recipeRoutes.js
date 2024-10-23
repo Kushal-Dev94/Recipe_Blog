61d7f68b4e18331495666ceb7897bbac23b5d755
Kushal-Dev94 <kushalkishor31@gmail.com>
2024-10-24 02:54:32 +0530

    first commit

---
+++ RecipeBlog-MongoDB-Node.js/server/routes/recipeRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const recipeController = require('../controllers/recipeController');
+
+
+/**
+ * App Routes 
+*/
+router.get('/', recipeController.homepage);
+router.get('/recipe/:id', recipeController.exploreRecipe );
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
+router.post('/search', recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
+router.get('/submit-recipe', recipeController.submitRecipe);
+router.post('/submit-recipe', recipeController.submitRecipeOnPost);
+
+router.delete('/delete-recipe/:id', recipeController.deleteRecipe);
+
+// About page
+router.get('/about', (req, res) => {
+    res.render('about', { title: 'About Us' });
+  });
+
+module.exports = router;
